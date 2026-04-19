21fcc47a6862a98c714515445ce57d140c3e7253
MQ <lookup1980@gmail.com>
2026-04-19 00:10:38 -0700

    Port TMA throughput benchmark for RTX 5090

--- tma_throughput/tma_tput.cu
+++ tma_throughput/tma_tput.cu
@@ -7,8 +7,6 @@
 namespace cg = cooperative_groups;
 namespace ptx = cuda::ptx;
 
-constexpr int32_t NUM_SMS = 148;                              // B200 has 148 SMs
-constexpr int32_t L2_SIZE = 132644864;                        // B200 L2 cache is 126.5 MiB
 constexpr size_t MAX_DATA_VOLUME = 2LL * 1024 * 1024 * 1024;  // 2 GB
 
 constexpr size_t alignDataVolume(size_t factor) {
@@ -28,6 +26,16 @@ constexpr size_t alignDataVolume(size_t factor) {
 #endif
 
 constexpr size_t ELEMS_PER_LOAD = LOAD_SIZE / sizeof(float);
+
+#define CUDA_CHECK(expr)                                                     \
+    do {                                                                     \
+        cudaError_t status__ = (expr);                                       \
+        if (status__ != cudaSuccess) {                                       \
+            fprintf(stderr, "CUDA error %s:%d: %s\n", __FILE__, __LINE__,    \
+                    cudaGetErrorString(status__));                           \
+            return 1;                                                        \
+        }                                                                    \
+    } while (0)
 
 
 __global__ void bulkAsyncCopyKernel(float *arr, size_t N) {
@@ -67,9 +75,21 @@ __global__ void bulkAsyncCopyKernel(float *arr, size_t N) {
     }
 }
 
+int get_device_attribute(cudaDeviceAttr attr, const char* name) {
+    int value = 0;
+    CUDA_CHECK(cudaDeviceGetAttribute(&value, attr, 0));
+    if (value <= 0) {
+        fprintf(stderr, "Invalid %s reported by device: %d\n", name, value);
+        std::exit(1);
+    }
+    return value;
+}
+
 
 int main() {
-    int32_t num_blks = NUM_SMS * CTAS_PER_SM;
+    int32_t num_sms = get_device_attribute(cudaDevAttrMultiProcessorCount, "SM count");
+    int32_t l2_size = get_device_attribute(cudaDevAttrL2CacheSize, "L2 size");
+    int32_t num_blks = num_sms * CTAS_PER_SM;
     size_t arr_size = alignDataVolume(num_blks * LOAD_SIZE * NUM_STAGES);
     size_t N = arr_size / sizeof(float);
 
@@ -80,20 +100,20 @@ int main() {
     }
 
     float *d_arr;
-    cudaMalloc(&d_arr, arr_size);
-    cudaMemcpy(d_arr, arr, arr_size, cudaMemcpyHostToDevice);
-    cudaDeviceSynchronize();
+    CUDA_CHECK(cudaMalloc(&d_arr, arr_size));
+    CUDA_CHECK(cudaMemcpy(d_arr, arr, arr_size, cudaMemcpyHostToDevice));
+    CUDA_CHECK(cudaDeviceSynchronize());
 
     void *flush_arr;
-    cudaMalloc(&flush_arr, L2_SIZE);
-    cudaMemset(flush_arr, 0xA5, L2_SIZE);
-    cudaDeviceSynchronize();
-    cudaFree(flush_arr);
+    CUDA_CHECK(cudaMalloc(&flush_arr, l2_size));
+    CUDA_CHECK(cudaMemset(flush_arr, 0xA5, l2_size));
+    CUDA_CHECK(cudaDeviceSynchronize());
+    CUDA_CHECK(cudaFree(flush_arr));
 
     bulkAsyncCopyKernel<<<num_blks, 1>>>(d_arr, N);
-    cudaDeviceSynchronize();
+    CUDA_CHECK(cudaDeviceSynchronize());
 
-    cudaFree(d_arr);
+    CUDA_CHECK(cudaFree(d_arr));
     free(arr);
     return 0;
 }
